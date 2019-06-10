863ddb97b7d626c61adab5ac7363bd0155d729c4
pando <spreadpando@gmail.com>
2019-06-09 21:48:12 -0600

    adding and purchasing products

--- server.js
+++ server.js
@@ -22,31 +22,83 @@ connection.connect(function (err) {
 
 // display available products
 let products = [];
-function renderStore() {
+function renderStore(_callback) {
     connection.query('SELECT * from products', function (err, result) {
+        console.log(`--------------------------------------------------------`);
         for (let i = 0; i < result.length; i++) {
-            console.log(`product key: ${result[i].item_id} | ${result[i].product_name} | $${result[i].price}`)
+            console.log(`product key: ${result[i].item_id} | ${result[i].product_name} | $${result[i].price} | x ${result[i].stock_quantity} in stock`)
             products.push(result[i]);
         };
-
+        console.log(`--------------------------------------------------------`);
+        _callback();
     });
 };
 
+function promptChoice() {
+    inquire.prompt([
+        { type: 'list', name: 'choice', message: 'which action would you like to take next?', choices: ['view/shop products', 'sell a product'] }
+    ]).then(function (response) {
+        let reply = response.choice;
+        switch (reply) {
+            case 'view/shop products':
+                renderStore(promptSale);
+                break;
+            case 'sell a product':
+                createProduct();
+                break;
+        }
+    })
+}
+
+function createProduct() {
+    inquire.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'what is the name of your product?',
+    },
+    {
+        type: 'list',
+        name: 'dept',
+        message: 'in which department would you like your item to be listed?',
+        choices: ['toys', 'games', 'electronics', 'books', 'kitchen', 'bathroom', 'consumable']
+    },
+    {
+        type: 'input',
+        name: 'price',
+        message: 'what price would you like to set? (in $USD)'
+    },
+    {
+        type: 'number',
+        name: 'quant',
+        message: 'how many products do you have in stock ready to ship?'
+    }]).then(function (rsp) {
+        let query = 'INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("' + rsp.name + '", "' + rsp.dept + '", "' + parseFloat(rsp.price) + '", "' + rsp.quant + '")';
+        connection.query(query, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(`
+            1 product inserted
+            `);
+            promptChoice();
+        })
+    })
+}
+
 function promptSale() {
     inquire.prompt([
-        { type: 'number', name: 'product_key', message: 'enter the product key you would like to order' },
+        { type: 'number', name: 'product_key', message: '\n enter the product key you would like to order' },
         { type: 'number', name: 'quantity', message: 'what quantity would you like to order?' }
     ]).then(function (response) {
         let pkey = response.product_key;
         let quantity = response.quantity;
         let found = false;
-        console.log(products);
         for (let i = 0; i < products.length; i++) {
             // check which product is selected
             if (products[i].item_id === pkey) {
                 found = true;
                 if (products[i].stock_quantity > quantity) {
-                    //place order
+                    //place order if available
                     let set = products[i].stock_quantity - quantity;
                     let where = 'item_id = ' + products[i].item_id;
                     let query = 'UPDATE products SET stock_quantity = ' + set + ' WHERE ' + where;
@@ -54,16 +106,23 @@ function promptSale() {
                         if (err) {
                             throw err;
                         }
-                        renderStore();
+                        console.log(`
+                        your order of :
+                         ${quantity} ${products[i].product_name}(s) 
+                        has been completed
+                        `);
+                        promptChoice();
                     })
                 } else {
                     console.log(':::::  out of stock  :::::');
+                    promptChoice();
                 }
             }
 
         }
         if (found === false) {
             console.log(':::::  that product key did not match any products  :::::');
+            promptSale();
         }
 
 
@@ -71,5 +130,5 @@ function promptSale() {
     })
 }
 
-renderStore();
-promptSale();
+
+promptChoice();
